75ed7ac73c309ce4b1ab33dd93ffb2c3deca8626
pidgay <kacper.goch.pasp@gmail.com>
2024-01-25 21:44:45 +0100

    Added tests, fixed InputValidation

--- src/test/java/workers/TestBackupService.java
+++ src/test/java/workers/TestBackupService.java
@@ -11,6 +11,10 @@ import workers.model.Employee;
 import workers.model.Salesman;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -61,6 +65,16 @@ public class TestBackupService {
 
     @Nested
     class TestDeserialization{
+
+        private void corruptFile(String filePath){
+            try {
+                FileWriter writer = new FileWriter(filePath);
+                writer.close();
+            } catch (Exception e){
+                System.out.println("Could not write to file");
+            }
+        }
+
         @BeforeEach
         public void setUp(){
             for (int i = 0; i < 10; i++){
@@ -75,19 +89,44 @@ public class TestBackupService {
                 );
                 Repository.addEmployee(salesman);
             }
-            String dirName = "TestDeserialization";
-            BackupService.start(true, true, dirName);
-            BackupService.start(true, false, dirName);
+            BackupService.start(true, true, "TestDeserializationGzip");
+            BackupService.start(true, false, "TestDeserializationZip");
         }
 
-        @Test
-        public void CorrectParallelDeserialization(){
+        @ParameterizedTest
+        @ValueSource(strings = {"TestDeserializationGzip", "TestDeserializationZip"})
+        public void SuccessfulParallelDeserialization(String dirName){
+            List<Employee> before = Repository.getEmployees();
+            Repository.getEmployees().clear();
 
+            BackupService.start(false, false, dirName);
+            for (int i = 0; i < before.size(); i++) {
+                assertEquals(before.get(i), Repository.getEmployees().get(i));
+            }
         }
 
-        @Test
-        public void IncorrectParallelDeserialization(){
+        @ParameterizedTest
+        @ValueSource(strings = {"TestDeserializationGzip", "TestDeserializationZip"})
+        public void UnsuccessfulParallelDeserialization(String dirName){
+            List<Employee> before = Repository.getEmployees();
+            Repository.getEmployees().clear();
+            String fileExtension;
+            if (dirName.contains("Gzip")) {
+                fileExtension = ".gz";
+            } else {
+                fileExtension = ".zip";
+            }
 
+            for (Employee employee : before) {
+                String filePath = dirName + "/" + employee.getPesel() + fileExtension;
+                corruptFile(filePath);
+            }
+
+            BackupService.start(false, false, dirName);
+
+            for (int i = 0; i < before.size(); i++) {
+                assertNotEquals(before.get(i), Repository.getEmployees().get(i));
+            }
         }
     }
 }
